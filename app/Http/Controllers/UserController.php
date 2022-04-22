f0e0a5da1a20cdd64426d444b5a4a4d918daa710
yjcodehub <lakshraj2121@gmail.com>
2022-04-22 22:31:35 +0530

    Login session created

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    function login(Request $req)
+    {
+        $user = User::where(['email'=>$req->email])->first();
+        //return $user;
+        if(!$user || !Hash::check($req->password, $user->password)){
+            return redirect('/login');
+        }
+        else{
+            $req->session()->put("user", $user);
+            return redirect('/');
+        }
+    }
+}
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,21 +5,20 @@
       <div class="col-8">
         <div class="card shadow">
           <div class="card-body">
-            <form>
+            <h4 class="text-center display-6 mb-5">Login Form</h4>
+            <form action="login" method="POST">
+              @csrf
               <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <label for="Email" class="form-label">Email address</label>
+                <input type="email" name="email" class="form-control" id="Email" aria-describedby="emailHelp"
+                  value="{{ old('email') }}">
                 <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
               </div>
               <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <label for="Password" class="form-label">Password</label>
+                <input type="password" name="password" class="form-control" id="Password">
               </div>
-              <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-              </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
+              <button type="submit" class="btn btn-primary">Login</button>
             </form>
           </div>
         </div>
