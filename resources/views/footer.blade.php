50fb436fdabd0b28e3986535ad9eea17682cd58e
yjcodehub <lakshraj2121@gmail.com>
2022-04-22 14:54:49 +0530

    Login page created

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+<!-- As a link -->
+<nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid justify-content-center">
+    <a class="navbar-brand fs-5" href="#">Copyright &copy; All rights are reserved from this site</a>
+  </div>
+</nav>
